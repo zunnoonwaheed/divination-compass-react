ce6f594d0b3523cca46d5b068aeadd2824c2641c
allinonebrandz <131856284+allinonebrandz@users.noreply.github.com>
2025-12-28 00:04:30 -0800

    Update main.py

--- main.py
+++ main.py
@@ -240,3 +240,4 @@ def natalchart_latlon(year: int, month: int, day: int, hour: int = 0, minute: in
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True, log_level="debug")
+    
