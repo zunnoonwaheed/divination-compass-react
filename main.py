475446d9882fb0bcb11b0c38833cc5fe1b8541ac
allinonebrandz <131856284+allinonebrandz@users.noreply.github.com>
2025-12-27 16:15:25 -0800

    Update main.py

--- main.py
+++ main.py
@@ -281,3 +281,6 @@ def natalchart_latlon(
         "bodies": bodies,
         "asteroids": minor,
     }
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
